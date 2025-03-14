27b10ac14ed86c96c67e6c00459fa1fdd449c765
CarVill30 <carlosrogervillarreal3@gmail.com>
2025-03-14 17:21:52 +0000

    mover los archivos index.html y app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,46 @@
-import "bootstrap";
-import "./style.css";
+function generateExcuse() {
+
+    const who = ['El perro', 'Mi hijo', 'Mi mujer', 'El gato'];
+
+    const action = ['tiró', 'mojó', 'aplastó', 'rompió'];
+
+    const what = ['mi trabajo', 'mi teléfono', 'el cuadro'];
+
+    const when = ['antes de la clase', 'cuando estaba durmiendo', 'mientras hacía ejercicio', 'durante mi almuerzo', 'mientras no estaba'];
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+    // Seleccionar un elemento aleatorio de cada arreglo
+
+    const randomWho = who[Math.floor(Math.random() * who.length)];
+
+    const randomAction = action[Math.floor(Math.random() * action.length)];
+
+    const randomWhat = what[Math.floor(Math.random() * what.length)];
+
+    const randomWhen = when[Math.floor(Math.random() * when.length)];
+
+
+    // Concatenar la excusa
+
+    return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+
+}
+
+
+// Establecer la excusa en el innerHTML del elemento HTML #excuse al cargar la página
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+    const excuseElement = document.getElementById('excuse');
+
+    if (excuseElement) {
+
+        excuseElement.innerHTML = generateExcuse();
+
+    } else {
+
+        console.error('El elemento con ID "excuse" no se encontró en el DOM.');
+
+    }
+
 };
