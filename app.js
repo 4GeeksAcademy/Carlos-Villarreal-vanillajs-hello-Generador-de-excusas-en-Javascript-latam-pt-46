d759a25a3eed456bffb3e2af0089a409e1ff9819
CarVill30 <carlosrogervillarreal3@gmail.com>
2025-03-14 13:25:52 +0000

    Cambie de usar el const en lugar del let

--- app.js
+++ app.js
@@ -1,20 +1,46 @@
 function generateExcuse() {
-    let who = ['El perro', 'Mi hijo', 'Mi mujer', 'El gato'];
-    let action = ['tiró', 'mojó', 'aplastó', 'rompió'];
-    let what = ['mi trabajo', 'mi teléfono', 'el cuadro'];
-    let when = ['antes de la clase', 'cuando estaba durmiendo', 'mientras hacía ejercicio', 'durante mi almuerzo', 'mientras no estaba'];
+
+    const who = ['El perro', 'Mi hijo', 'Mi mujer', 'El gato'];
+
+    const action = ['tiró', 'mojó', 'aplastó', 'rompió'];
+
+    const what = ['mi trabajo', 'mi teléfono', 'el cuadro'];
+
+    const when = ['antes de la clase', 'cuando estaba durmiendo', 'mientras hacía ejercicio', 'durante mi almuerzo', 'mientras no estaba'];
+
 
     // Seleccionar un elemento aleatorio de cada arreglo
-    let randomWho = who[Math.floor(Math.random() * who.length)];
-    let randomAction = action[Math.floor(Math.random() * action.length)];
-    let randomWhat = what[Math.floor(Math.random() * what.length)];
-    let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+    const randomWho = who[Math.floor(Math.random() * who.length)];
+
+    const randomAction = action[Math.floor(Math.random() * action.length)];
+
+    const randomWhat = what[Math.floor(Math.random() * what.length)];
+
+    const randomWhen = when[Math.floor(Math.random() * when.length)];
+
 
     // Concatenar la excusa
+
     return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+
 }
 
+
 // Establecer la excusa en el innerHTML del elemento HTML #excuse al cargar la página
+
 window.onload = function() {
-    document.getElementById('excuse').innerHTML = generateExcuse();
+
+    const excuseElement = document.getElementById('excuse');
+
+    if (excuseElement) {
+
+        excuseElement.innerHTML = generateExcuse();
+
+    } else {
+
+        console.error('El elemento con ID "excuse" no se encontró en el DOM.');
+
+    }
+
 };
